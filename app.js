934a8552efcb3f9bccca6a88e21229693d0aef5a
asifiqbalsagor <asifiqbalsagor98@gmail.com>
2022-09-03 19:33:15 +0600

    card and js content added

--- app.js
+++ app.js
@@ -10,5 +10,26 @@ const loadCategoryName = async () => {
 };
 
 
+const displayAllCategoryName = (categories) => {
+  const categoryContainer = document.getElementById("category-container");
+  categories.forEach((category) => {
+    const { category_id, category_name } = category;
+    const categoryDiv = document.createElement("div");
+    categoryDiv.innerHTML = `
+          
+          <div class="">
+          <li class="nav-item mx-3">
+            <a onclick="loadNewsByCategory('${category_id}')" class="nav-link active" aria-current="page" href="#">${category_name}</a>
+          </li>
+        </div>
+          
+          `;
+    categoryContainer.appendChild(categoryDiv);
+  });
+};
+
+
+
+
 
 loadCategoryName();
