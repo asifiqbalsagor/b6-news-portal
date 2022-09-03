8c902e6561baaefaa148f171252293d8f1ecbb25
asifiqbalsagor <asifiqbalsagor98@gmail.com>
2022-09-03 19:25:24 +0600

    header added

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-console.log(144343)
+const loadCategoryName = async () => {
+  const url = `https://openapi.programming-hero.com/api/news/categories`;
+  try {
+    const res = await fetch(url);
+    const data = await res.json();
+    displayAllCategoryName(data.data.news_category);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+
+
+loadCategoryName();
